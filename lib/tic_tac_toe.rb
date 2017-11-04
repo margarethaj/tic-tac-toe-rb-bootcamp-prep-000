20a4209e1572df3e778bb2d4d49974a0ba468492
Margaretha Juan <tulip_holy@yahoo.com>
2017-11-04 21:23:58 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,16 +37,19 @@ def position_taken?(board, index)
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if !valid_move?(board, index)
-    turn(board)
+  isValid = false
+  while isValid == false
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    isValid = valid_move?(board, index)
+    if isValid
+      board = move(board, index, current_player(board))
+      display_board(board)
+    end
   end
-  move(board, index, current_player(board))
-  display_board(board)
+  return board
 end
-
 
 def turn_count(board)
   counter = 0
@@ -129,7 +132,7 @@ end
 
 def play(board)
   until over?(board) == true
-    board = turn(board)
+    turn(board)
     gameOver = over?(board)
   end
   if winner(board).nil? == false
